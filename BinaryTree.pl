7a179db041d56e3edf1b95b5636b6c8a4f6c3523
Caelan <infamouspot@gmail.com>
2018-12-16 14:33:31 +0000

    Update BinaryTree.pl

--- BinaryTree.pl
+++ BinaryTree.pl
@@ -8,3 +8,8 @@ bTree(tree(N,Left,nil)) :- Left =< N.
 bTree(tree(N,nil,Right)) :- N < Right.
 bTree(tree(_,Left,Right)) :- bTree(Left), bTree(Right).
 bTree(tree(N,Left,Right)) :- Left =< N, N < Right.
+
+/* preorder traversal */
+preorder(T,L) :- bTree(T), bTree(T(N,Left,Right)), append(N,[Left|Right],L).
+preorder(T,L) :- bTree(T(_,Left,_)), preorder(Left,L).
+preorder(T,L) :- bTree(T(_,_,Right)), preorder(Right,L).
