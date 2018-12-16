80a56391e0e1b105648ff6a251c5e54de59e2519
Caelan <infamouspot@gmail.com>
2018-12-16 14:25:31 +0000

    Update BinaryTree.pl

--- BinaryTree.pl
+++ BinaryTree.pl
@@ -1,2 +1,10 @@
 /* tree predicates */
-emptyBT = nil
+istree(nil).
+istree(tree(_,Left,Right)) :- istree(Left), istree(Right).
+
+emptyBT = nil.
+bTree(tree(_,nil,nil)).
+bTree(tree(N,Left,nil)) :- Left =< N.
+bTree(tree(N,nil,Right)) :- N < Right.
+bTree(tree(_,Left,Right)) :- bTree(Left), bTree(Right).
+bTree(tree(N,Left,Right)) :- Left =< N, N < Right.
